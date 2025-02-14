2e0cd322a0b1ed2ad986c101533d3509f0bcdfd9
Md Abu Omayer Babu <abuomayerbabu@gmail.com>
2025-02-14 19:53:56 +0600

    add api testing file

---
+++ test.py
@@ -0,0 +1,8 @@
+import google.generativeai as genai
+from config import GEMINI_API_KEY
+
+genai.configure(api_key=GEMINI_API_KEY)
+
+model = genai.GenerativeModel("gemini-pro")
+response = model.generate_content("What is Google AI?")
+print(response.text)
